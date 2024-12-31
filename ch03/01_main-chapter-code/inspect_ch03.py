681a44224f9ccdf068067ad0ec0f59a3a9da5e0f
neowell <neowell@126.com>
2024-12-31 11:53:17 +0800

    some notes followed last commits

--- ch03/01_main-chapter-code/inspect_ch03.py
+++ ch03/01_main-chapter-code/inspect_ch03.py
@@ -17,28 +17,65 @@ def inspect_navie_self_attention_single() -> None:
        [0.77, 0.25, 0.10], # one      (x^5)
        [0.05, 0.80, 0.55]] # step     (x^6)
     )
-    print(f'\ninputs:\n{inputs}\n')
+    print(f'\n===>S1-inputs:\n{inputs}\n')
 
     # 2. Taking the second embedding("journey") as the example query
     query = inputs[1]
     print(f"inputs' shape: {inputs.shape}")
     print(f"query's shape: {query.shape}\n")
-    # using the query to perform dot product with every embedding in inputs
-    # thus you get attention score between every embedding and query
+    """
+    using the query to perform dot product with every embedding in inputs.
+    thus you get attention score between every embedding and query
+    by applying dot product, the relevance of "journey" with other words in sequence can be known.
+    bigger value means bigger relevance.
+    """
     attention_score_x2 = torch.matmul(inputs, query)
+    print(f'===>S2-attention score:\n{attention_score_x2}\n')
+    print(f"attention's shape: {attention_score_x2.shape}\n")
 
     # 3. normalize our attension_score, to make the sum of its elemnts to 1.
-    # such normalization will imporve our training's performance.
-    # here we apply softmax to do the normalization.
-    normalized_attention_score_x2 = torch.softmax(attention_score_x2, dim=0)
-    print(f'normalized_attention_score_x2 attention score:\n{normalized_attention_score_x2}')
-    print(f'sum = {normalized_attention_score_x2.sum()}')
+    """
+    such normalization will imporve our training's performance.
+    here we apply softmax to do the normalization.
+    as been mentioned in the second part, the results of dot product means the relevance,
+    by normalize their value, this can also be called as "weight" in a way,
+    the weight of every word towards x2, including x2 self.
+    so in the result, you will find the second value is the biggest, as of course,
+    what is most related to x2 is x2 itself.
+    """
+    weights_to_x2 = torch.softmax(attention_score_x2, dim=0)
+    print(f'===>S3-weights_to_x2 :\n{weights_to_x2}')
+    print(f"normalized attention's shape: {weights_to_x2.shape}")
+    print(f'sum = {weights_to_x2.sum()}')
 
     # 4. compute context vector z2
     print(f"inputs' shape: {inputs.shape}")
-    print(f"normalized_attention_score's shape: {normalized_attention_score_x2.shape}")
-    context_vector = torch.matmul(normalized_attention_score_x2, inputs)
-    print(context_vector)
+    print(f"normalized_attention_score's shape: {weights_to_x2.shape}")
+    """
+    after getting the weights from step3, we now multiply every weight to its corresponding input,
+    as the following:
+        y1 = w1 * x1 = (w1*a1, w1*b1, w1*c1)
+        y2 = w2 * x2 = (w2*a2, w2*b2, w2*c2)
+        ...
+        y6 = w6 * x6 = (w6*a6, w6*b6, w6*c6)
+        new weighted input = {y1, y2, y3, y4, y5, y6}^T
+
+    for now, this is like we've got a weighted(or scaled) inputs in the perspective of x2.
+    then, we add those inputs by row, thus we get the context vector z2:
+        z21 = w1*a1 + w2*a2 + ... + w6*a6
+        z22 = w1*b1 + w2*b2 + ... + w6*b6
+        z23 = w1*b1 + w2*c2 + ... + w6*c6
+        z2 = {z21, z22, z23}
+
+    so here why do we add the weighted inputs by row? 
+    this is beacause we need to get a "summary" of query(x2 here), this sum is including how every other words
+    contributes thier relevance, for a sum, the bigger relevance word will surely contribute more for this value.
+    and this final context vector is a new representaiton for x2, which is granted with more info like position,
+    other words' relations with it.
+    """
+    context_vector = torch.matmul(weights_to_x2, inputs)
+    print(f'===>S4-context_vector:\n{context_vector}\n')
+    print(f"context_vector's shape: {context_vector.shape}\n")
 
 
 if __name__ == '__main__':
